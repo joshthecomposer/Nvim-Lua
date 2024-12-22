b7c6e8dd94147d5b682313f7e1b89cbfaf62aaed
joshthecomposer <jdwisecomposer@gmail.com>
2024-12-22 02:09:29 -0600

    this is a commit message

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -6,8 +6,14 @@ vim.api.nvim_set_keymap('n', '<leader>ai', '<cmd>lua vim.lsp.buf.code_action()<C
 vim.cmd [[
 	autocmd BufWritePre *.js,*.jsx,*.ts,*.tsx lua vim.lsp.buf.format({timeout_ms = 1000})
 ]]
-vim.api.nvim_set_keymap('n', '<leader>t', ':split | terminal<CR>', { noremap = true, silent = true })
-vim.keymap.set('t', '<esc><esc>', [[<c-\><c-n>]], { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>bd', ':bd!<CR>', {noremap = true, silent = true})
+
+vim.api.nvim_create_autocmd("TermOpen", {
+  callback = function()
+    vim.keymap.set('n', '<leader>t', ':bd!<CR>', { noremap = true, silent = true, buffer = true })
+    vim.keymap.set('t', '<esc><esc>', [[<c-\><c-n>]], { noremap = true, silent = true, buffer = true })
+  end,
+})
+
+vim.keymap.set('n', '<leader>t', ':split | terminal<CR>', { noremap = true, silent = true })
 
 vim.cmd([[autocmd TermOpen * startinsert]])
